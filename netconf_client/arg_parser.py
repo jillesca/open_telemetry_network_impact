354286b7b26825c4c75b37b8daab21c441a53d98
jillesca <jillesca@cisco.com>
2023-08-12 15:52:55 +0100

    added argsparse group for new xpath filter

--- netconf_client/arg_parser.py
+++ netconf_client/arg_parser.py
@@ -3,15 +3,18 @@ import argparse
 
 def get_arg_parser():
     parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
     parser.add_argument(
         "--device_settings",
         default="netconf_devices_settings.json",
         help="Device Settings in json fomat",
     )
-    parser.add_argument(
+    group.add_argument(
         "--xml_filter",
-        required=True,
-        # default="netconf_interface_stats.xml",
         help="Netconf Filter to apply in xml format",
+    )
+    group.add_argument(
+        "--xpath_filter",
+        help="Netconf Filter to apply in xpath format, see https://github.com/ncclient/ncclient/pull/405 for an example",
     )
     return parser
