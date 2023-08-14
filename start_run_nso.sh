4c7295c633804fe1e88cff30b8991fb85422affd
developer <developer@devbox.lab.devnetsandbox.local>
2023-08-14 07:42:30 -0700

    updating start_run_nso.sh

--- start_run_nso.sh
+++ start_run_nso.sh
@@ -1,3 +1,6 @@
+# /bin/bash
+set -o xtrace
+
 source ~/nso/ncsrc
 
 ncs-setup --package ~/nso/packages/neds/cisco-ios-cli-6.91/ --dest ~/nso-instance
@@ -8,12 +11,12 @@ ncs
 
 ncs --status | grep -i status
 
-ncs_load -F c -lm nso/base_config.conf
+ncs_load -F c -lm ~/open_telemetry_network/nso/base_config.conf
 
 echo "devices connect" | ncs_cli -C -u admin
 
 echo "devices sync-from" | ncs_cli -C -u admin
 
-ncs_load -F c -lm nso/cat8000v-0.conf
+ncs_load -F c -lm ~/open_telemetry_network/nso/cat8000v-0.conf
 
-ncs_load -F c -lm nso/cat8000v-1.conf
+ncs_load -F c -lm ~/open_telemetry_network/nso/cat8000v-1.conf
