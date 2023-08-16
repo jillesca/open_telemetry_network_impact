11ddcc87da421aeee6fb99a26237d91dbb1bc779
jillesca <jillesca@cisco.com>
2023-08-16 18:55:29 +0100

    added sample alterting to grafana

--- build_run_grafana.sh
+++ build_run_grafana.sh
@@ -6,6 +6,7 @@ source .env.local
 
 docker build --file grafana/dockerfile \
         --build-arg GRAFANA_TOKEN=$GRAFANA_TOKEN \
+        --build-arg GRAFANA_WEB_HOOK=$GRAFANA_WEB_HOOK \
         --tag grafana:$GRAFANA_TAG .
 
 docker run -itd -p 3000:3000 --name grafana --add-host host.docker.internal:host-gateway grafana:$GRAFANA_TAG 
