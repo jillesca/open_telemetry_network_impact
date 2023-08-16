e281785e4ddfee4b79e4274f39ec8db44a8f85ec
jillesca <jillesca@cisco.com>
2023-08-16 12:23:24 +0100

    added isis count interfaces up filter

--- netconf_client/parsers/cisco_ios_xe_isis_oper.py
+++ netconf_client/parsers/cisco_ios_xe_isis_oper.py
@@ -1,43 +1,51 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from netconf_parsers import Parser
 from netconf_devices import netconf_device
+
+FIELD_CODE = "isis_interface_state"
 
 
 @dataclass
 class isis_stats_iosxe_oper(Parser):
     device: netconf_device = None
-    interfaces_adj_up_count: int = 0
+    isis_interfaces_adj_up: int = 0
+    stats: list = field(default_factory=list)
 
     def parse(self, data: dict, device: netconf_device) -> list[dict]:
         self.device = device
-        return self.neighbor_stats(data)
+        self.neighbor_stats(data)
+        return self.stats
 
-    def neighbor_stats(self, data: dict) -> list[dict]:
-        stats: list = []
-        neighbors: dict = data["rpc-reply"]["data"]["isis-oper-data"]["isis-instance"]
+    def neighbor_stats(self, data: dict) -> None:
+        isis_instances: dict = data["rpc-reply"]["data"]["isis-oper-data"][
+            "isis-instance"
+        ]
 
-        for neighbor in neighbors:
-            if "isis-neighbor" in neighbor and "isis-adj-up" in neighbor["state"]:
-                self.interfaces_adj_up_count += 1 
-    
-        for neighbor in neighbors:
-            if "isis-neighbor" in neighbor:
-                metadata = self.metadata(neighbor)
-                stats.append(metadata)
-        return stats
+        for key, value in isis_instances.items():
+            if "isis-neighbor" in key:
+                self.count_adjcencies(value)
+
+        for key, value in isis_instances.items():
+            if "isis-neighbor" in key:
+                self.gather_metadata(value)
+
+    def count_adjcencies(self, instance: list) -> None:
+        for neighbor in instance:
+            if "isis-adj-up" in neighbor["state"]:
+                self.isis_interfaces_adj_up += 1
+
+    def gather_metadata(self, instance: list) -> None:
+        for neighbor in instance:
+            self.stats.append(self.metadata(neighbor))
 
     def metadata(self, neighbor: dict) -> dict:
         return {
-            "operational_status": 1
-            if "isis-adj-up" in neighbor["state"]
-            else 0,
-            "name": neighbor["system-id"].replace(" ", "_"),
-            "field": "neighbor_statistics",
+            "isis_adjancecy_status": 1 if "isis-adj-up" in neighbor["state"] else 0,
+            "neighbor_id": neighbor["system-id"].replace(" ", "_"),
+            "field": FIELD_CODE,
             "device": self.device.hostname,
             "ip": self.device.host,
-            "if-name":neighbor["if-name"],
-            "ipv4-address":neighbor["ipv4-address"],
-            "interfaces_adj_up_count": self.interfaces_adj_up_count,
+            "interface_name": neighbor["if-name"],
+            "ipv4_address": neighbor["ipv4-address"],
+            "isis_interfaces_adj_up": self.isis_interfaces_adj_up,
         }
-
-# python3 main.py --device_settings cat8000v-0_settings.json --xml_filter Cisco-IOS-XE-isis-oper.xml
