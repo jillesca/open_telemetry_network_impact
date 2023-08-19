e9d0579da5340b3e9e501b297ae53ed3eb78bbdb
jillesca <jillesca@cisco.com>
2023-08-19 12:34:30 +0100

    added post method for alerts route, parse json to dict and return json

--- langchain/test.py
+++ langchain/test.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 import os
+import json
 
 
 app = Flask(__name__)
 LLM_API_KEY = os.environ["LLM_API_KEY"]
 
 
-@app.route("/")
-def index():
-    return "it works again one more time, testing!!!!"
+@app.route("/alerts", methods=["POST"])
+def alerts():
+    if request.method == "POST":
+        data = json.loads(request.args.get("data"))
+        print(data, flush=True)
+        return jsonify(data)
 
 
 if __name__ == "__main__":
