a2922ca9eb40a2600bd3cd1c03babf1d13b68281
jillesca <jillesca@cisco.com>
2023-08-25 11:55:09 +0100

    ai can remember conversation but crashes with too many alerts

--- langchain/app.py
+++ langchain/app.py
@@ -3,7 +3,6 @@ import json
 import logging
 from flask import Flask, request, jsonify, g
 from llm_bot import LLM_Chatbot
-from langchain.chains import LLMChain
 
 
 try:
@@ -15,45 +14,36 @@ logging.basicConfig(filename="flask.log", level=logging.DEBUG)
 
 app = Flask(__name__)
 
+chatbot = LLM_Chatbot()
 
-def talk_to_chat(chatbot: LLMChain, data: str) -> dict:
+
+def talk_to_chat(data: str) -> dict:
     return chatbot.conversations(data)
 
 
-@app.route("/webhook", methods=["POST"])
-def alerts():
-    if request.method == "POST":
-        print("data sent to /webhook")
-        data = json.loads(request.args.get("data"))
-        analyse = ""
-        analyse += json.dumps(data["commonLabels"])
-        analyse += json.dumps(data["title"])
-        analyse += json.dumps(data["state"])
-        analyse += json.dumps(data["message"])
-        analyse += json.dumps(data["commonAnnotations"])
-        chatbot = LLM_Chatbot()
-        answer = talk_to_chat(chatbot, analyse)
-        print(f"LLM answered: {answer['text']}", flush=True)
-        return jsonify(answer["text"])
+def process_webhook(data: dict) -> None:
+    analyse = ""
+    analyse += json.dumps(data["commonLabels"])
+    analyse += json.dumps(data["title"])
+    analyse += json.dumps(data["state"])
+    analyse += json.dumps(data["message"])
+    analyse += json.dumps(data["commonAnnotations"])
+    print(f"{analyse}=")
+    answer = talk_to_chat(analyse)
+    print(f"LLM answered: {answer['text']}", flush=True)
 
 
 @app.route("/", methods=["POST"])
-def alert2():
+def receive_webhook():
     if request.method == "POST":
-        print("data sent to /")
-        print(request.__dict__)
+        print(f"{request.json=}")
+        data = request.json
 
-        data = json.loads(request.args.get())
-        analyse = ""
-        analyse += json.dumps(data["commonLabels"])
-        analyse += json.dumps(data["title"])
-        analyse += json.dumps(data["state"])
-        analyse += json.dumps(data["message"])
-        analyse += json.dumps(data["commonAnnotations"])
-        chatbot = LLM_Chatbot()
-        answer = talk_to_chat(chatbot, analyse)
-        print(f"LLM answered: {answer['text']}", flush=True)
-        return jsonify(answer["text"])
+        # Grafana sends empty request to validate the webhook.
+        if "firing" in data["status"]:
+            process_webhook(data)
+
+        return jsonify(success=False)
 
 
 if __name__ == "__main__":
